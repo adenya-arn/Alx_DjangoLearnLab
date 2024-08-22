88f63af97d892363df46e03f6dbc61ebb9bcefe2
Arnold Adenya <aadenya7@gmail.com>
2024-08-22 17:42:40 +0300

    Question 0 done

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,60 @@
+import os
+import django
+import sys
+
+# Add the project root directory to the sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Set up Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
+django.setup()
+
+
+from relationship_app.models import Author, Book, Library, Librarian
+"""
+def create_author():
+    Instance = str(input("Add authors name:"))
+    add = Author.objects.create(name = Instance)
+    create_Book(add)
+
+
+def create_Book(author):
+    Book.objects.create(title = str(input("Title of book is:")), author = author) 
+    create_library()
+
+
+def create_library():
+    Name = str(input("Name of library;"))
+   # book_name = str(input("Name of Book to add:"))
+    Library.objects.create(name = Name) #ADA'books = book_name'
+    create_librarian()
+
+def create_librarian():
+    Name = str(input("Librarian's name is:"))
+    library = str(input("Working in which library:"))
+    Librarian.objects.create(name = Name, library = library)  
+
+create_author()"""
+
+
+
+
+#Query all books by a specific author.
+
+author_name = str(input("Name of author whose books you want:"))
+authors = Author.objects.get(name = author_name )
+books_by_author = Book.objects.filter(author = authors)
+print(f"books by author/n {books_by_author} ")
+
+
+#List all books in a library.
+
+lib_name = str(input("Name of library:"))
+books_by_libarary = Library.objects.get(name = lib_name)
+print(books_by_libarary)
+
+#Retrieve the librarian for a library.
+
+librarian_name = str(input("Name of library:"))
+library_name = Librarian.objects.get(library = lib_name)
+print(library_name)
