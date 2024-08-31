1840d1ef8877a7afeb283cb3a3fee31b127079c2
adenya-arn <aadenya7@gmail.com>
2024-08-31 20:28:52 +0300

    ....

--- advanced_features_and_security/relationship_app/models.py
+++ advanced_features_and_security/relationship_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.conf import settings
 
 # Create your models here.
@@ -54,10 +54,27 @@ class UserProfile(models.Model):
         return f'{self.user.username} - {self.role}'
     
 
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, email, password, **extra_fields)
+
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField()
-    profile_photo = models.ImageField()
+    profile_photo = models.ImageField(null=True, blank=True)
 
+    objects = CustomUserManager()
     def __str__(self):
         return self.date_of_birth
-
