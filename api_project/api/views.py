604a22b9aed10644d2c659c07dea89ed4129d09b
adenya-arn <aadenya7@gmail.com>
2024-09-01 10:42:14 +0300

    Error on quiz 1

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerilizer
-from rest_framework import generics, viewsets
+from rest_framework import viewsets
+from rest_framework import generics
 
 # Create your views here.
 
 class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerilizer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        name_filter = self.request.query_params.get('name', None)
+
+        if name_filter is not None:
+            queryset =queryset.filter(name__icontains=name_filter)
+
+        return queryset
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
